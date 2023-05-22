035b4a37d1bfb2a7f9b173ef57718a7148d2a2db
felipe <gael.ferreira505@gmail.com>
2023-05-22 10:14:35 -0300

    form

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import {useForm} from 'react-hook-form';
+import React from 'react'
+import './App.css'
+
+const  App = () => {
+  const {register,handleSubmit}=  useForm();
+
+
+  const createSala = (dados) => {
+    event.preventDefault();
+  }
+
+  return (
+    <>
+       <form onSubmit={handleSubmit(createSala)}>
+        <h3>Cadastro de Sala</h3>
+       <label htmlFor='sala_nome'>Nome da sala</label>
+       <input  
+       type='text' 
+       id='sala_nome'
+       {...register("nome")}
+        required />
+       <label htmlFor='sala_status'>Status</label>
+       <select id='sala status'>
+       <option value={1}>livre</option>
+       <option value={2}>Ocupada</option>
+       </select>
+       <button type='submit'>Enviar</button>
+       </form>
+    </>
+  )
+}
+
+export default App
